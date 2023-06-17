721b81d7d66552cb9168e7e42edd1b5d65886dc6
soriful15 <sorifulislam.rs10@gmailcomm>
2023-06-18 00:05:54 +0600

    recheck

--- src/Page/MyClass/MyClass.jsx
+++ src/Page/MyClass/MyClass.jsx
@@ -28,73 +28,73 @@ const MyClass = () => {
     return (
         <>
 
-<Helmet>
-        <title>Sport Academic | My Class  Page</title>
-      </Helmet>
+            <Helmet>
+                <title>Sport Academic | My Class  Page</title>
+            </Helmet>
             <h1 className='lg:text-3xl text-lg font-bold my-3 text-center'>Hello <span className='text-violet-700'>{user && user.displayName}</span> Welcome To DashBoard</h1>
             <div className="border-t-4 border-orange-700 mb-5 w-3/4 mx-auto "></div>
 
 
 
 
-          <div>
-              <div className="overflow-x-auto w-full">
-                <table className="table">
-                    {/* head */}
-                    <thead className='bg-gradient-to-r from-violet-500 to-fuchsia-500 text-black'>
-                        <tr>
-                            <th>
-                                #
-                            </th>
-                            <th>Class Image</th>
-                            <th>Class Name</th>
-                            <th>Status</th>
-                            <th>Total Enrolled</th>
-                            <th>Price</th>
-                            <th>AvailAble Seat</th>
-                            <th>Feedback</th>
-                            <th>Updated</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* row 1 */}
-                        {
-                            allClasses.map((allClass, index) =>
-                                <tr key={allClass._id}>
-                                    <th>
-                                        {index + 1}
-                                    </th>
-                                    <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={allClass.classesImg} alt="Avatar Tailwind CSS Component" />
+            <div>
+                <div className="overflow-x-auto w-full">
+                    <table className="table">
+                        {/* head */}
+                        <thead className='bg-gradient-to-r from-violet-500 to-fuchsia-500 text-black'>
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>Class Image</th>
+                                <th>Class Name</th>
+                                <th>Status</th>
+                                <th>Total Enrolled</th>
+                                <th>Price</th>
+                                <th>AvailAble Seat</th>
+                                <th>Feedback</th>
+                                <th>Updated</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                allClasses.map((allClass, index) =>
+                                    <tr key={allClass._id}>
+                                        <th>
+                                            {index + 1}
+                                        </th>
+                                        <td>
+                                            <div className="flex items-center space-x-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src={allClass.classesImg} alt="Avatar Tailwind CSS Component" />
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td className='text-base font-bold'>
-                                        {allClass.classesName}
-                                    </td>
-                                    <td className='text-yellow-400'>{allClass.status}</td>
-                                    <td className='text-pink-800'> {allClass.enroll ? allClass.enroll : "Not Enroll"}</td>
-                                    <td className='text-green-800'>${allClass.price}</td>
-                                    <td className='text-indigo-700'>{allClass.seats}</td>
-                                    <td>
-                                        {allClass.feedBack ? allClass.feedBack : "Nothing FeedBack"}
-                                    </td>
-                                    <Link to={`/dashboard/allClasses/${allClass._id}`} className="btn btn-info btn-xs">Updated</Link>
+                                        </td>
+                                        <td className='text-base font-bold'>
+                                            {allClass.classesName}
+                                        </td>
+                                        <td className='text-yellow-400'>{allClass.status}</td>
+                                        <td className='text-pink-800'> {allClass.enroll ? allClass.enroll : "Not Enroll"}</td>
+                                        <td className='text-green-800'>${allClass.price}</td>
+                                        <td className='text-indigo-700'>{allClass.seats}</td>
+                                        <td>
+                                            {allClass.feedBack ? allClass.feedBack : "Nothing FeedBack"}
+                                        </td>
+                                        <Link to={`/dashboard/allClasses/${allClass._id}`} className="btn btn-info btn-xs">Updated</Link>
 
 
-                                </tr>
+                                    </tr>
 
-                            )
-                        }
+                                )
+                            }
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
 
 
         </>
